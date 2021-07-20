b5cb14767fc6f61d20c0b99420fdac37b3c5f459
yingwinwin <1245839794@qq.com>
2021-07-21 01:26:02 +0800

    轮播图

--- docs/Carousel/index.jsx
+++ docs/Carousel/index.jsx
@@ -56,3 +56,56 @@ const handleMouseLeave = () =>{
 };
 
 export default Carousel;
+
+
+import React, {useRef, useEffect, useState } from "react";
+import img0 from "./img/0.jpg";
+import img1 from "./img/1.jpg";
+import img2 from "./img/2.jpg";
+import img3 from "./img/3.jpg";
+import img4 from "./img/4.jpg";
+import './index.less'
+
+const scrollArr = [img0, img1, img2, img3, img4];
+const Scroll = () => {
+    const [left, setLeft] = useState(0);
+    const [ind, setInd] = useState(0);
+    const imgRef = useRef(null);
+    const scrollWarp = useRef(null);
+    useEffect(() => {
+        
+        setLeft()
+        console.log(scrollWarp.current?.scrollWidth/scrollArr.length);
+        return () => {
+            
+        }
+    }, [scrollWarp, imgRef])
+
+  return <div className="scroll-wrap">
+      <div className="scroll-left" ref={scrollWarp} >
+         <div className="ul" style={{left: `${(-scrollWarp.current?.offsetWidth) * (ind >= scrollArr.length ? 0 : ind)}px`}} >
+         {
+              scrollArr.map(item => {
+                  return <img key={item} src={item} key={item} />
+              })
+          }
+        <img ref={imgRef} src={scrollArr[0]} />
+         </div>
+        <div style={{position: 'absolute'}}>
+        {
+            scrollArr.map((item, index) => {
+                return <div key={item} onClick={() => {
+                    setInd(index)
+                }}>{item}</div>
+            })
+        }
+        </div>
+      </div>
+      <div className="scroll-right">
+
+      </div>
+  </div>;
+};
+
+export default Scroll;
+
