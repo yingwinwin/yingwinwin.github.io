922400c4c600c4f7d26ad07f22a23f2f76d4536c
yingwinwin <1245839794@qq.com>
2021-12-07 00:54:35 +0800

    fix: img路径

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function Home() {
       description="Description will go into a meta tag in <head />"
     >
       <div  className={styles.hero_wrap} style={{
-        background: `url(../../static/img/bg.svg) center center/cover no-repeat`
+        background: `url(./img/bg.svg) center center/cover no-repeat`
       }}>
         <h1 className={styles.hero_title}>{siteConfig.title} 前端 开发 javaScript 个人记录 博客</h1>
         <p className={styles.hero_subtitle}>{siteConfig.tagline}</p>
